220ef7912d90f6a0b8ee6c116275935d6e399b94
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-08-06 12:30:55 +0530

    added production grade server

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 
 # Check for the existence of the migrations directory
 if [ -d "migrations" ]; then
+  cd src/
   # Migrations directory exists, run the application directly
-  python -m flask --app src/app run --port 5000 --debug
+  python3 -m gunicorn --worker-class gevent --workers 3 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5
+
 else
   # Migrations directory doesn't exist, initialize and apply migrations
   flask --app src/app db init
@@ -12,7 +14,9 @@ else
 
   # Check if migrations were successful (optional)
   if [ $? -eq 0 ]; then
-    python -m flask --app src/app run --port 5000 --debug
+    cd src/
+    python3 -m gunicorn --worker-class gevent --workers 3 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5
+
   else
     echo "Migrations failed. Check logs for details."
     exit 1
