d2427f9d6c089ce4d8f797688f27cbbd89fb1636
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-08-06 14:26:25 +0530

    added reffrence - id and by coloumn

--- src/app/Models/UserInfo_Model.py
+++ src/app/Models/UserInfo_Model.py
@@ -5,6 +5,8 @@ class UserInfo(db.Model):
 
    __tablename__ = "user_info"
    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+   reff_id = db.Column(db.String,nullable=False,unique=True)
+   reff_by = db.Column(db.String,nullable=True)
    mobile_no = db.Column(db.String, nullable=False,unique=True)   
    name = db.Column(db.String, nullable=False)
    sate = db.Column(db.String, nullable=False)
@@ -12,5 +14,5 @@ class UserInfo(db.Model):
    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 def __repr__(self):
-    return f'<User Info mobile_no="{self.mobile_no}", name="{self.name}", sate={self.sate}, pin_code="{self.pin_code}", timestamp="{self.timestamp}">'
+   return f'<User Info:\nrefference_id="{self.reff_id}\n",refference_by="{self.reff_by}"\n,mobile_no="{self.mobile_no}\n",name="{self.name}\n",state={self.sate}\n,pin_code="{self.pin_code}\n",timestamp="{self.timestamp}\n">'
 
