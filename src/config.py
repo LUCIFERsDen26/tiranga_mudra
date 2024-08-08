2f6cdc48f5c788aa9bc509dc197b8e9ae784e55c
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-08-08 13:56:43 +0530

    config update : - dev uses sqlite - production mysql db

--- src/config.py
+++ src/config.py
@@ -7,12 +7,22 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-
+  
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
-        or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = "mrfrIMEngCl0pAKqWIIBS_g"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    ENV = "development"
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_ECHO = True
-    
+
+
+class ProductionConfig(Config):
+    ENV = "production"
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://website:website@user_info_db:3306/user_info'
+    SQLALCHEMY_ECHO = False
 
