b0b800f09812a2bb201e176a7432ec324b39fa03
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-08-12 14:16:13 +0530

    changed for new page

--- src/app/routes/reference_route.py
+++ src/app/routes/reference_route.py
@@ -4,4 +4,4 @@ bp = Blueprint("reference_page", __name__)
 
 @bp.route("/reference/<ref_id>")
 def reference_page(ref_id):
-    return render_template("/refer_page/index.html",show_done=False,show_form=True,count_value=get_user_info_count(),ref_id=ref_id)
+    return render_template("/refer_page/index.html",count_value=get_user_info_count(),ref_id=ref_id)
