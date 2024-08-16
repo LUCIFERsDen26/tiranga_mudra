6ba1b48e1591bdfcc516ad7a9bfb2fbc1da8388a
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-08-16 13:07:33 +0530

    start.sh can start app in debug mode

--- start.sh
+++ start.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Function to start Flask application in debug mode
+start_flask_debug() {
+  # Overwrite .env file with the debug configuration
+  echo "CONFIGURATION_SETUP=\"config.DevelopmentConfig\"" > src/.env
+  echo "Running Flask application in debug mode..."
+
+  # Run Flask application with --debug and --reload
+  flask --app src/app run --debug --reload
+}
+
+# Function to start Flask application with Gunicorn
+start_flask_production() {
+  # Overwrite .env file with the debug configuration
+  echo "CONFIGURATION_SETUP=\"config.ProductionConfig\"" > src/.env
+  echo "Starting Flask application with Gunicorn..."
+
+  # Start the Flask application with Gunicorn
+  cd src/
+  python3 -m gunicorn --worker-class gevent --workers 3 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5
+}
+
 # Check if .env file exists in src directory
 if [ ! -f src/.env ]; then
   # Copy example.env to src directory
@@ -9,6 +30,9 @@ else
   echo ".env file already exists in src directory"
 fi
 
-# Start the Flask application with Gunicorn
-cd src/
-python3 -m gunicorn --worker-class gevent --workers 3 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5
+# Check if the 'debug' argument is passed
+if [ "$1" == "debug" ]; then
+  start_flask_debug
+else
+  start_flask_production
+fi
